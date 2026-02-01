fcab61be2dafdfeaf04f9e5bd8d8b0da85cbbaf1
Yash2-27 <yashkadam27@gtasterix.com>
2026-02-01 11:43:24 +0530

    Clean project structure and remove IDE metadata

---
+++ CategoryProductTest/src/main/java/com/NimapJavaTestApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimapJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimapJavaTestApplication.class, args);
+	}
+}
